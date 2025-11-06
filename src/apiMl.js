766744511dff4d9d9000722b8384a9575563ba2f
Somu Sharma <somusharmass.9450@gmail.com>
2025-11-06 16:23:05 +0530

    final

--- src/apiMl.js
+++ src/apiMl.js
@@ -9,9 +9,7 @@ const mlApi = axios.create({
   },
 });
 
-/* This function is correct. It prepares the data for the frontend.
-  We will use these keys to map to the backend keys in our API calls.
-*/
+
 export const getSongData = (mlSong) => {
   return {
     id: mlSong.track_id || mlSong._id || mlSong.songId, 
@@ -19,35 +17,29 @@ export const getSongData = (mlSong) => {
     artist: mlSong.artists || mlSong.artist,
     image: mlSong.img || mlSong.coverImage,
     src: mlSong.preview || mlSong.src,
-    album_name: mlSong.album_name || mlSong.album, // Make sure this is included
+    album_name: mlSong.album_name || mlSong.album,
   };
 };
 
-/**
- * NEW FUNCTION
- * Fetches popular songs and extracts a unique list of artists from them.
- * This avoids hardcoding artist names and provides a dynamic list.
- */
+
 export const getArtistData = async (limit = 20) => {
   try {
     // Fetch a larger list of popular songs to get a good variety of artists
-    const response = await mlApi.get('/popular?limit=100');
+    const response = await mlApi.get('/popular?limit=5000');
     const songs = response.data;
 
     const artistMap = new Map();
 
-    // Create a map of unique artists, using the first image found for them
     for (const song of songs) {
       if (song.artists && !artistMap.has(song.artists)) {
         artistMap.set(song.artists, {
-          type: 'artist', // Set type for LibraryGridMenu
+          type: 'artist',
           name: song.artists,
-          image: song.img || '/logo192.png', // Use song image as artist image
+          image: song.img || '/logo192.png',
         });
       }
     }
 
-    // Return an array of artist objects, respecting the limit
     return Array.from(artistMap.values()).slice(0, limit);
 
   } catch (err) {
